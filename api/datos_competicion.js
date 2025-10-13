3231eb2fbbf7fc735a20eb3c7154f9969402d1d1
avelinogalan-ux <avelinogalan@gmail.com>
2025-10-13 17:33:21 +0200

    Create datos_competicion.js

---
+++ api/datos_competicion.js
@@ -0,0 +1,12 @@
+{
+  "disciplina": "Competición de Tiro",
+  "datos": [
+    {
+      "puesto": 1,
+      "nombre": "Ejemplo Competidor",
+      "series": [10, 9, 10],
+      "total": 29,
+      "xTotal": 2
+    }
+  ]
+}
