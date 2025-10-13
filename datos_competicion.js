f7d61a2ccc7da988e51448093490ce25efa657d4
avelinogalan-ux <avelinogalan@gmail.com>
2025-10-13 17:36:07 +0200

    Create datos_competicion.js

---
+++ datos_competicion.js
@@ -0,0 +1,40 @@
+{
+  "disciplina": "Rifle Aire 10m",
+  "datos": [
+    {
+      "puesto": 1,
+      "nombre": "Ana García",
+      "series": [100, 99, 100, 100, 99, 100],
+      "total": 598,
+      "xTotal": 45
+    },
+    {
+      "puesto": 2,
+      "nombre": "Carlos López",
+      "series": [99, 100, 99, 98, 100, 99],
+      "total": 595,
+      "xTotal": 42
+    },
+    {
+      "puesto": 3,
+      "nombre": "María Rodríguez",
+      "series": [98, 99, 98, 99, 98, 99],
+      "total": 591,
+      "xTotal": 38
+    },
+    {
+      "puesto": 4,
+      "nombre": "David Martínez",
+      "series": [97, 98, 97, 98, 97, 98],
+      "total": 585,
+      "xTotal": 35
+    },
+    {
+      "puesto": 5,
+      "nombre": "Laura Sánchez",
+      "series": [96, 97, 96, 97, 96, 97],
+      "total": 579,
+      "xTotal": 32
+    }
+  ]
+}
